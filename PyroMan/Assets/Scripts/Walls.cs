0ab7f2286a6276313209feb37f08919aa445eba0
Martichoras <emil@rhoeg.dk>
2014-03-05 17:12:01 +0100

    Physical settings and wall-script (updated)
    added solid wall to both scene and script + imported Maya bomb to Unity
    project

--- PyroMan/Assets/Scripts/Walls.cs
+++ PyroMan/Assets/Scripts/Walls.cs
@@ -4,7 +4,8 @@ using System.Collections;
 
 public class Walls : MonoBehaviour {
 	
-	public Transform Wall;									
+	public Transform Wall;
+	public Transform Wall_solid;
 	
 	private const int xSize = 19; 
 	private const int zSize = 19;
@@ -17,11 +18,11 @@ public class Walls : MonoBehaviour {
 		{ 1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1 },
 		{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
 		{ 1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1 },
-		{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
-		{ 1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1 },
-		{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
-		{ 1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1 },
-		{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
+		{ 1,0,0,0,0,0,0,2,2,2,2,2,0,0,0,0,0,0,1 },
+		{ 1,0,1,0,1,0,1,2,1,2,1,2,1,0,1,0,1,0,1 },
+		{ 1,0,0,0,0,0,0,2,2,0,2,2,0,0,0,0,0,0,1 },
+		{ 1,0,1,0,1,0,1,2,1,2,1,2,1,0,1,0,1,0,1 },
+		{ 1,0,0,0,0,0,0,2,2,2,2,2,0,0,0,0,0,0,1 },
 		{ 1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1 },
 		{ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 },
 		{ 1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1 },
@@ -38,8 +39,11 @@ public class Walls : MonoBehaviour {
 		for (int z=0 ; z<zSize ; z++){
 			for (int x=0 ; x<xSize ; x++){
 				if(bluePrint[z,x] == 1)
+					maze[z,x] = Instantiate(Wall_solid, new Vector3(x*2.0f-xSize+1, 0.0f, -z*2.0f+zSize-1), Quaternion.identity) as GameObject;
+	
+				if(bluePrint[z,x] == 2)
 					maze[z,x] = Instantiate(Wall, new Vector3(x*2.0f-xSize+1, 0.0f, -z*2.0f+zSize-1), Quaternion.identity) as GameObject;
-				
+
 
 			}
 		}
