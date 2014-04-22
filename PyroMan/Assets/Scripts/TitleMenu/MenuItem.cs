a13b25c3f09c640f6842c7c17fba903e4e8e9af0
Xerillio <cas0493@gmail.com>
2014-04-22 22:17:36 +0200

    Titlescreen fix for issue #13 - part 1
    Title screen added with 3 menu buttons (start, instructions and exit).
    Instructions has no functionality yet.
    The title screen is missing some design too.

---
+++ PyroMan/Assets/Scripts/TitleMenu/MenuItem.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuItem : MonoBehaviour {
+
+	/// <summary>
+	/// The z-position of the item when not selected.
+	/// </summary>
+	private static float inactiveZ = 0;
+	/// <summary>
+	/// The z-position of the item when selected.
+	/// </summary>
+	private static float activeZ = -0.5f;
+	/// <summary>
+	/// The time the animation takes in seconds.
+	/// </summary>
+	private static float animationSpeed = 5.0f;
+
+	/// <summary>
+	/// True if the item is currently selected, otherwise false.
+	/// </summary>
+	private bool on = false;
+	/// <summary>
+	/// True if the animation of the item is done so the user can choose another button.
+	/// </summary>
+	private bool isAnimDone = true;
+	public bool IsAnimDone {
+		get { return this.isAnimDone; }
+	}
+
+	/// <summary>
+	/// Toggles the state of the menu item, which will start an animation.
+	/// </summary>
+	/// <param name="on">If true, the menu item will move towards the screen. Reverse if false.</param>
+	public void OnSelect(bool on) {
+		this.on = on;
+		this.isAnimDone = false;
+		this.enabled = true;
+	}
+
+	void Update() {
+		// Calculate the position at which this item is moving towards
+		Vector3 destination = this.transform.position;
+		if (this.on) {
+			destination.z = MenuItem.activeZ;
+		}
+		else {
+			destination.z = MenuItem.inactiveZ;
+		}
+		// Move it
+		this.transform.position = Vector3.Lerp(this.transform.position, destination, Time.deltaTime * MenuItem.animationSpeed);
+		// If close enough, allow user to select another item
+		float distance = (this.transform.position - destination).magnitude;
+		if (distance < 0.1f) {
+			this.isAnimDone = true;
+			// If position is practically reached, stop this animation and set position of this item to the destination.
+			if (distance < 0.01f) {
+				this.transform.position = destination;
+				this.enabled = false;
+			}
+		}
+	}
+}
---
+++ PyroMan/Assets/Scripts/TitleMenu/MenuManager.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	public enum ItemTypes { Start, Instructions, Exit };
+	public ItemTypes[] types;
+	public MenuItem[] items;
+	private int currentlySelected = 0;
+	private int lastSelected = 0;
+
+	// Use this for initialization
+	void Start () {
+		this.items[this.currentlySelected].OnSelect(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.items[this.currentlySelected].IsAnimDone) {
+			float v = Input.GetAxis("Vertical"); // Up and down to move up and down
+			float h = Input.GetAxis("Horizontal"); // Left and right to move up and down respectively
+			this.lastSelected = this.currentlySelected;
+
+			// v-h means that if up and right are pressed at the same time, the selection won't change as right means down (so, up & down == 0)
+			if (v-h > 0.1f) { // Up pressed
+				this.currentlySelected--;
+				if (this.currentlySelected < 0)
+					this.currentlySelected = 0;
+			}
+			else if (v-h < -0.1f) { // Down pressed
+				this.currentlySelected++;
+				if (this.currentlySelected >= this.items.Length)
+					this.currentlySelected = this.items.Length - 1;
+			}
+
+			// Call the objects being deselected and selected respectively.
+			if (this.currentlySelected != this.lastSelected) {
+				this.items[this.lastSelected].OnSelect(false);
+				this.items[this.currentlySelected].OnSelect(true);
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter)) {
+			switch (this.types[this.currentlySelected]) {
+				case ItemTypes.Start:
+					Application.LoadLevel("GameScreen");
+					break;
+				case ItemTypes.Instructions:
+					// Load an instructions screen or in someway present instructions
+					break;
+				case ItemTypes.Exit:
+					Application.Quit();
+					break;
+				default:
+					break;
+			}
+		}
+	}
+}
