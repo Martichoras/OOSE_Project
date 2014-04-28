102ddce68b3f0953847e886c6125072224502484
Martichoras <emil@rhoeg.dk>
2014-04-28 14:34:00 +0200

    Issue #12.2
    Added script-templates for powerups

---
+++ PyroMan/Assets/Scripts/PowerupFirepower.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupFirepower : MonoBehaviour {
+	
+	void OnCollisionEnter (Collision collision) {
+		collision.collider
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
