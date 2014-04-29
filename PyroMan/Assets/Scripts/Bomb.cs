61f4df01a8489fa5b24c7b390e531653d7c404b2
Xerillio <cas0493@gmail.com>
2014-04-29 12:51:26 +0200

    Fixed #23 - Explosion adjustment and modelling
    Bug fix: Bombs will now explode each other and power ups.
    Modelling fixing: Pivot points are adjusted so spawning of the models
    will be more precise.

--- PyroMan/Assets/Scripts/Bomb.cs
+++ PyroMan/Assets/Scripts/Bomb.cs
@@ -14,6 +14,7 @@ public class Bomb : MonoBehaviour {
 	private int x;
 	private int z;
 	private BombBag playerBag;
+	private bool isExploded = false;
 
 	public GameObject ExplosionPrefab;
 
@@ -31,15 +32,19 @@ public class Bomb : MonoBehaviour {
 
 	}
 
-	private void Explode(){
-		float up, down, left, right;
+	public void Explode(){
+		if (this.isExploded)
+			return;
 		
+		this.isExploded = true;
+
+		float up, down, left, right;
 		float unit = LevelGenerator.gameUnit;
 		// Check how far we can explode in each direction
-		right = this.HandleHitObject(this.transform.position, new Vector3(1, 0, 0), this.explodeRange * unit);
-		left = this.HandleHitObject(this.transform.position, new Vector3(-1, 0, 0), this.explodeRange * unit);
-		up = this.HandleHitObject(this.transform.position, new Vector3(0, 0, 1), this.explodeRange * unit);
-		down = this.HandleHitObject(this.transform.position, new Vector3(0, 0, -1), this.explodeRange * unit);
+		right = this.ExplodeInDirection(this.transform.position, new Vector3(1, 0, 0), this.explodeRange * unit);
+		left = this.ExplodeInDirection(this.transform.position, new Vector3(-1, 0, 0), this.explodeRange * unit);
+		up = this.ExplodeInDirection(this.transform.position, new Vector3(0, 0, 1), this.explodeRange * unit);
+		down = this.ExplodeInDirection(this.transform.position, new Vector3(0, 0, -1), this.explodeRange * unit);
 
 		// Animate the explosion with some spawned particles
 		Instantiate(ExplosionPrefab, this.transform.position, Quaternion.identity);
@@ -66,7 +71,10 @@ public class Bomb : MonoBehaviour {
 		playerBag.AfterExplosion (x,z);
 	}
 
-	private float HandleHitObject(Vector3 origin, Vector3 direction, float distance) {
+	private float ExplodeInDirection(Vector3 origin, Vector3 direction, float distance) {
+		if (distance <= 0.0f)
+			return 0;
+
 		RaycastHit hit;
 		if (!Physics.Raycast(origin, direction, out hit, distance))
 			return distance;
@@ -82,10 +90,15 @@ public class Bomb : MonoBehaviour {
 		}
 		else if (obj.tag == "Player" || obj.tag == "PowerUp") {
 			Destroy(obj);
-			return (origin-pos).magnitude + this.HandleHitObject(pos, direction, distance - (origin-pos).magnitude); // Test how much further we can explode
+			return (origin-pos).magnitude + this.ExplodeInDirection(pos, direction, distance - (origin-pos).magnitude); // Test how much further we can explode
+		}
+		else if (obj.tag == "Bomb") {
+			Bomb bomb = obj.GetComponent<Bomb>();
+			bomb.Explode();
+			return (origin - pos).magnitude + this.ExplodeInDirection(pos, direction, distance - (origin - pos).magnitude); // Test how much further we can explode
 		}
 		else { // We must have hit a wall
-			return (origin-pos).magnitude - LevelGenerator.gameUnit; // Return the distance to the position before the wall (the wall can't explode)
+			return (origin - pos).magnitude - LevelGenerator.gameUnit; // Return the distance to the position before the wall (the wall can't explode)
 		}
 		
 	}
