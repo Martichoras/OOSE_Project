b73101d8622f6b8b6c8cf6558b57638ced2eb9e4
Xerillio <cas0493@gmail.com>
2014-04-30 11:02:53 +0200

    Some comments added and fixing gameoverscreen
    Added comments to some code.
    Game over screen fixed (aspect ratio was wrong)

--- PyroMan/Assets/Scripts/MenuItem.cs
+++ PyroMan/Assets/Scripts/MenuItem.cs
@@ -3,13 +3,17 @@ using System.Collections;
 
 public class MenuItem : MonoBehaviour {
 
+	/// <summary>
+	/// The distance between the two points used to distinguish whether the item is active (selected) or not.
+	/// </summary>
 	private static float animMoveDist = 0.5f;
 	/// <summary>
-	/// The time the animation takes in seconds.
+	/// The time the animation should take in seconds.
 	/// </summary>
 	private static float animationSpeed = 5.0f;
-
-
+	/// <summary>
+	/// Destination of the animation (the point at which the item should stop the animation)
+	/// </summary>
 	private Vector3 dest;
 	/// <summary>
 	/// True if the animation of the item is done so the user can choose another button.
@@ -20,23 +24,15 @@ public class MenuItem : MonoBehaviour {
 	}
 
 	/// <summary>
-	/// Toggles the state of the menu item, which will start an animation.
+	/// Called immediately after instantiation of the object
 	/// </summary>
-	/// <param name="on">If true, the menu item will move towards the screen. Reverse if false.</param>
-	public void OnSelect(bool on) {
-		if (on)
-			this.dest = this.transform.position + this.transform.InverseTransformDirection(Vector3.back) * MenuItem.animMoveDist;
-		else
-			this.dest = this.transform.position + this.transform.InverseTransformDirection(Vector3.forward) * MenuItem.animMoveDist;
-		
-		this.isAnimDone = false;
-		this.enabled = true;
-	}
-
 	void Awake() {
 		this.enabled = false;
 	}
 
+	/// <summary>
+	/// Called every frame
+	/// </summary>
 	void Update() {
 		
 		// Move it
@@ -51,5 +47,19 @@ public class MenuItem : MonoBehaviour {
 				this.enabled = false;
 			}
 		}
+	}
+
+	/// <summary>
+	/// Toggles the state of the menu item, which will start an animation.
+	/// </summary>
+	/// <param name="on">If true, the menu item will move towards the screen. Reverse if false.</param>
+	public void OnSelect(bool on) {
+		if (on)
+			this.dest = this.transform.position + this.transform.InverseTransformDirection(Vector3.back) * MenuItem.animMoveDist;
+		else
+			this.dest = this.transform.position + this.transform.InverseTransformDirection(Vector3.forward) * MenuItem.animMoveDist;
+		
+		this.isAnimDone = false;
+		this.enabled = true;
 	}
 }
--- PyroMan/Assets/Scripts/MenuManager.cs
+++ PyroMan/Assets/Scripts/MenuManager.cs
@@ -2,22 +2,51 @@
 using System.Collections;
 
 public class MenuManager : MonoBehaviour {
-
+	
+	/// <summary>
+	/// Enum of button types on the menu.
+	/// </summary>
 	public enum ItemTypes { Start, Restart, MainMenu, Instructions, Exit };
+	/// <summary>
+	/// Array of button types for current shown menu.
+	/// </summary>
 	public ItemTypes[] types;
+	/// <summary>
+	/// Menu items shown (an item is a button).
+	/// </summary>
 	public MenuItem[] items;
+	/// <summary>
+	/// Array index of the currently selected item.
+	/// </summary>
 	private int currentlySelected = 0;
+	/// <summary>
+	/// Previous selected item.
+	/// </summary>
 	private int lastSelected = 0;
+	/// <summary>
+	/// Determines when the camera animation (rotation) is done.
+	/// </summary>
 	private bool camIsRotated = true;
+	/// <summary>
+	/// Speed of the rotation animation.
+	/// </summary>
 	private float rotSpeed = 50.0f;
+	/// <summary>
+	/// Determines whether the instructions screen is shown.
+	/// </summary>
 	private bool showingInstructions = false;
 
-	// Use this for initialization
+	/// <summary>
+	/// Called when the object is instantiated.
+	/// </summary>
 	void Start () {
+		// Selects the first item on load.
 		this.items[this.currentlySelected].OnSelect(true);
 	}
 	
-	// Update is called once per frame
+	/// <summary>
+	/// Called once per frame.
+	/// </summary>
 	void Update () {
 		if (!this.showingInstructions && this.items[this.currentlySelected].IsAnimDone) {
 			float v = Input.GetAxis("Vertical"); // Up and down to move up and down
@@ -43,11 +72,14 @@ public class MenuManager : MonoBehaviour {
 			}
 		}
 
+		// Select the item
 		if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter)) {
+			// Go back to main menu if instructions are shown
 			if (this.showingInstructions) {
 				this.camIsRotated = false;
 			}
 			else {
+				// Check what item is selected and make an action accordingly
 				switch (this.types[this.currentlySelected]) {
 					case ItemTypes.Start:
 					case ItemTypes.Restart:
@@ -74,12 +106,18 @@ public class MenuManager : MonoBehaviour {
 			}
 		}
 
+		// Animate camera rotation
 		if (!this.camIsRotated) {
 			this.RotateCamera(this.showingInstructions ? ItemTypes.MainMenu : ItemTypes.Instructions);
 		}
 	}
 
+	/// <summary>
+	/// Rotating the camera a percentage between two fixed points each frame, when the animation is activated.
+	/// </summary>
+	/// <param name="screen">The screen to show (instructions or main menu)</param>
 	private void RotateCamera(ItemTypes screen) {
+		// Calculate target rotation
 		Quaternion newRot = Camera.main.transform.rotation;
 		if (screen == ItemTypes.Instructions) {
 			newRot = Quaternion.Euler(63.34f, 0, 0);
@@ -88,6 +126,7 @@ public class MenuManager : MonoBehaviour {
 			newRot = Quaternion.Euler(3.115f, 0, 0);
 		}
 		
+		// Rotate. If rotation reached target, stop further rotation
 		if ((Camera.main.transform.rotation = Quaternion.RotateTowards(Camera.main.transform.rotation, newRot, this.rotSpeed * Time.deltaTime)) == newRot) {
 			this.camIsRotated = true;
 			this.showingInstructions = (screen == ItemTypes.Instructions) ? true : false;
